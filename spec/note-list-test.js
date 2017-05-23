f071e9485ec1faeb8a6b2c4cd7757dbd0f449705
Sophie Mann <sklm@live.co.uk>
2017-05-23 15:06:23 +0100

    adds multiple note storage check

--- spec/note-list-test.js
+++ spec/note-list-test.js
@@ -10,6 +10,14 @@
     assert.isTrue(noteList.getNotes()[0].constructor === note.constructor);
   }
 
+  function storesMultipleNotes() {
+    var noteList = new NoteList();
+    noteList.createNote(string);
+    noteList.createNote("string");
+    assert.isTrue(noteList.getNotes()[1].getText() === "string");
+  }
+
   createNewNote();
+  storesMultipleNotes();
 
 })(this);
